4f09009e50f57f208faade09b307461914042762
ZHANG Zui <zhangzui1989@gmail.com>
2020-06-28 02:41:41 +0200

    2020/06/27

---
+++ src/main/java/com/example/leetcode/biweeklycontest/contest29/AverageSalary.java
@@ -0,0 +1,20 @@
+package com.example.leetcode.biweeklycontest.contest29;
+
+public class AverageSalary {
+    public static void main(String[] args) {
+
+    }
+
+    public double average(int[] salary) {
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < salary.length; i++) {
+            sum += salary[i];
+            min = Math.min(min,salary[i]);
+            max = Math.max(max,salary[i]);
+        }
+        return (double) (sum - min - max) / (salary.length - 2);
+    }
+}
