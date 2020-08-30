551bd20ac93d87cf60f437a55adf3ac7c230c403
zhangzui <zhangzui1989@gmail.com>
2020-08-31 01:12:31 +0200

    2020/08/31

---
+++ src/main/java/com/example/leetcode/challenge/August/week4/SumLeftLeaves.java
@@ -0,0 +1,85 @@
+package com.example.leetcode.challenge.August.week4;
+
+/**
+ * Find the sum of all left leaves in a given binary tree.
+ *
+ * Example:
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+ */
+public class SumLeftLeaves {
+    public static void main(String[] args) {
+
+    }
+
+    int res;
+    public int sumOfLeftLeaves(TreeNode root) {
+        res = 0;
+        if(root == null)
+            return 0;
+        helper(root,false);
+        return res;
+    }
+
+    public void helper(TreeNode node,boolean isLeft){
+        if(node.left == null && node.right == null && isLeft)
+            res += node.val;
+        if(node.left !=null)
+            helper(node.left,true);
+        if(node.right !=null)
+            helper(node.right,false);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode convert(Integer[] array) {
+        int floor = 0, count = 0;
+        TreeNode[] treeNodes = new TreeNode[array.length];
+        while (array != null && count < array.length) {
+            int start = (int) Math.pow(2, floor) - 1;
+            int end = (int) Math.pow(2, floor + 1) - 1;
+            if (end > array.length) {
+                end = array.length;
+            }
+            for (int i = start; i < end; i++) {
+                if (array[i] != null) {
+                    treeNodes[i] = new TreeNode(array[i]);
+                } else {
+                    treeNodes[i] = null;
+                }
+                if (i > 0) {
+                    int parent = (i - 1) / 2;
+                    if (parent >= 0) {
+                        TreeNode pNode = treeNodes[parent];
+                        if (pNode != null) {
+                            if (i % 2 == 1) {
+                                pNode.left = treeNodes[i];
+                            } else {
+                                pNode.right = treeNodes[i];
+                            }
+                        }
+                    }
+                }
+                count++;
+            }
+            floor++;
+        }
+        return treeNodes[0];
+    }
+}
---
+++ src/main/java/com/example/leetcode/weeklycontest/test204/MaximumLengthSubarrayWithPositiveProduct.java
@@ -0,0 +1,70 @@
+package com.example.leetcode.weeklycontest.test204;
+
+/**
+ * Given an array of integers nums, find the maximum length of a subarray where the product of all its elements is positive.
+ *
+ * A subarray of an array is a consecutive sequence of zero or more values taken out of that array.
+ *
+ * Return the maximum length of a subarray with positive product.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,-2,-3,4]
+ * Output: 4
+ * Explanation: The array nums already has a positive product of 24.
+ * Example 2:
+ *
+ * Input: nums = [0,1,-2,-3,-4]
+ * Output: 3
+ * Explanation: The longest subarray with positive product is [1,-2,-3] which has a product of 6.
+ * Notice that we cannot include 0 in the subarray since that'll make the product 0 which is not positive.
+ * Example 3:
+ *
+ * Input: nums = [-1,-2,-3,0,1]
+ * Output: 2
+ * Explanation: The longest subarray with positive product is [-1,-2] or [-2,-3].
+ * Example 4:
+ *
+ * Input: nums = [-1,2]
+ * Output: 1
+ * Example 5:
+ *
+ * Input: nums = [1,2,3,5,-6,4,0,10]
+ * Output: 4
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ */
+public class MaximumLengthSubarrayWithPositiveProduct {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,-2,-3,0,1};
+        MaximumLengthSubarrayWithPositiveProduct maximumLengthSubarrayWithPositiveProduct = new MaximumLengthSubarrayWithPositiveProduct();
+        maximumLengthSubarrayWithPositiveProduct.getMaxLen(nums);
+    }
+
+    public int getMaxLen(int[] nums) {
+        int res = 0;
+        int length = nums.length;
+        int countN;
+
+        for (int i = 0; i < length - res; i++) {
+            countN = 0;
+            for (int j = i; j < length; j++) {
+                if(nums[j] == 0){
+                    break;
+                } else{
+                    if(nums[j] < 0)
+                        countN++;
+                    if(countN % 2 ==0)
+                        res = Math.max(res,j-i+1);
+                }
+            }
+        }
+        return res;
+    }
+}
