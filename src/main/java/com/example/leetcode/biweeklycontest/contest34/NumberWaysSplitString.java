d53050327daba76c0a6d651267cef96e234b15c0
zhangzui <zhangzui1989@gmail.com>
2020-09-07 10:05:35 +0200

    2020/09/07

---
+++ src/main/java/com/example/leetcode/biweeklycontest/contest34/NumberWaysSplitString.java
@@ -0,0 +1,110 @@
+package com.example.leetcode.biweeklycontest.contest34;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a binary string s (a string consisting only of '0's and '1's), we can split s into 3 non-empty strings s1, s2, s3 (s1+ s2+ s3 = s).
+ *
+ * Return the number of ways s can be split such that the number of characters '1' is the same in s1, s2, and s3.
+ *
+ * Since the answer may be too large, return it modulo 10^9 + 7.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "10101"
+ * Output: 4
+ * Explanation: There are four ways to split s in 3 parts where each part contain the same number of letters '1'.
+ * "1|010|1"
+ * "1|01|01"
+ * "10|10|1"
+ * "10|1|01"
+ * Example 2:
+ *
+ * Input: s = "1001"
+ * Output: 0
+ * Example 3:
+ *
+ * Input: s = "00000"
+ * Output: 3
+ * Explanation: There are three ways to split s in 3 parts.
+ * "0|0|00"
+ * "0|00|0"
+ * "00|0|0"
+ * Example 4:
+ *
+ * Input: s = "100100010100110"
+ * Output: 12
+ *
+ *
+ * Constraints:
+ *
+ * s[i] == '0' or s[i] == '1'
+ * 3 <= s.length <= 10^5
+ */
+public class NumberWaysSplitString {
+    public static void main(String[] args) {
+        NumberWaysSplitString numberWaysSplitString =  new NumberWaysSplitString();
+        int res = numberWaysSplitString.numWays("100100010100110");
+        System.out.println(res);
+    }
+
+    public int numWays(String s) {
+        int count1 = 0;
+        int length = s.length();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < length; i++) {
+            if(chars[i] == '1'){
+                count1++;
+            }
+        }
+        if(count1 % 3 != 0)
+            return 0;
+        else if(count1 == 0) {
+            long res = 0;
+            for (int i = 1; i < length - 1 ; i++) {
+                res += i;
+                res = res % 1000000007;
+            }
+            return (int)res;
+        } else {
+            int size = count1/3;
+            int a = -1,b = -1;
+            int resa = 1,resb = 1;
+            int index = 0;
+            for (int i = 0; i < length; i++) {
+                if(chars[i] == '1'){
+                    index++;
+                    if(index == size){
+                        a = i;
+                    }
+                    if(index == size + 1){
+                        resa = i - a;
+                    }
+                    if(index == size * 2){
+                        b = i;
+                    }
+                    if(index == size * 2 + 1){
+                        resb = i - b;
+                    }
+                }
+            }
+            Stack<Integer> stack = new Stack<>();
+            int res = 0;
+            int val;
+            while (resb > 0){
+                val = resb%10;
+                resb = resb/10;
+                stack.add(resa * val);
+            }
+            while (!stack.isEmpty()){
+                res = res * 10 + stack.pop();
+                res = res % 1000000007;
+            }
+            return res;
+        }
+    }
+}
---
+++ src/main/java/com/example/leetcode/weeklycontest/test205/NumberWays.java
@@ -0,0 +1,76 @@
+package com.example.leetcode.weeklycontest.test205;
+
+import java.util.*;
+
+/**
+ * Given two arrays of integers nums1 and nums2, return the number of triplets formed (type 1 and type 2) under the following rules:
+ *
+ * Type 1: Triplet (i, j, k) if nums1[i]2 == nums2[j] * nums2[k] where 0 <= i < nums1.length and 0 <= j < k < nums2.length.
+ * Type 2: Triplet (i, j, k) if nums2[i]2 == nums1[j] * nums1[k] where 0 <= i < nums2.length and 0 <= j < k < nums1.length.
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [7,4], nums2 = [5,2,8,9]
+ * Output: 1
+ * Explanation: Type 1: (1,1,2), nums1[1]^2 = nums2[1] * nums2[2]. (4^2 = 2 * 8).
+ * Example 2:
+ *
+ * Input: nums1 = [1,1], nums2 = [1,1,1]
+ * Output: 9
+ * Explanation: All Triplets are valid, because 1^2 = 1 * 1.
+ * Type 1: (0,0,1), (0,0,2), (0,1,2), (1,0,1), (1,0,2), (1,1,2).  nums1[i]^2 = nums2[j] * nums2[k].
+ * Type 2: (0,0,1), (1,0,1), (2,0,1). nums2[i]^2 = nums1[j] * nums1[k].
+ * Example 3:
+ *
+ * Input: nums1 = [7,7,8,3], nums2 = [1,2,9,7]
+ * Output: 2
+ * Explanation: There are 2 valid triplets.
+ * Type 1: (3,0,2).  nums1[3]^2 = nums2[0] * nums2[2].
+ * Type 2: (3,0,1).  nums2[3]^2 = nums1[0] * nums1[1].
+ * Example 4:
+ *
+ * Input: nums1 = [4,7,9,11,23], nums2 = [3,5,1024,12,18]
+ * Output: 0
+ * Explanation: There are no valid triplets.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 1 <= nums1[i], nums2[i] <= 10^5
+ */
+public class NumberWays {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,1};
+        int[] nums2 = new int[]{1,1,1};
+        NumberWays numberWays = new NumberWays();
+        numberWays.numTriplets(nums1,nums2);
+    }
+
+    public int numTriplets(int[] nums1, int[] nums2) {
+        int res =  helper(nums1,nums2) + helper(nums2,nums1);
+        return res;
+    }
+
+    public int helper(int[] nums1, int[] nums2){
+        int res = 0;
+        if(nums1.length <2){
+            return res;
+        }
+        Map<Long,Integer> map = new HashMap<>();
+        for(int i=0; i<nums1.length-1; i++){
+            for(int j=i+1; j<nums1.length; j++){
+                long tmp = (long)nums1[i] * (long)nums1[j];
+                map.put(tmp, map.getOrDefault(tmp,0)+1);
+            }
+        }
+        for(int i=0; i<nums2.length; i++){
+            long tmp = (long)nums2[i] * (long)nums2[i];
+            if(map.containsKey(tmp)){
+                res += map.get(tmp);
+            }
+        }
+        return res;
+    }
+}
